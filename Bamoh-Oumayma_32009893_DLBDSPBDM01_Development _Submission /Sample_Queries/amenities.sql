18c654535ce7423b437ff55bfd0dbe81b3d9ba1a
oumayma <134213098+Oumaymabamoh@users.noreply.github.com>
2023-09-08 00:20:57 +0100

    Delete Bamoh-Oumayma_32009893_DLBDSPBDM01_Development _Submission /Sample_Queries directory

--- Bamoh-Oumayma_32009893_DLBDSPBDM01_Development _Submission /Sample_Queries/amenities.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM Airbnb.amenities
-LIMIT 10;
--- Bamoh-Oumayma_32009893_DLBDSPBDM01_Development _Submission /Sample_Queries/average_rating.sql
+++
@@ -1,7 +0,0 @@
-SELECT u.first_name, u.last_name,
-ROUND(AVG(rr.rating), 2) AS "Average_rating"
-FROM Airbnb.review_rating rr
-JOIN Airbnb.user u ON rr.user_id = u.user_id
-GROUP BY u.user_id
-ORDER BY AVG(rr.rating) DESC
-LIMIT 12;
